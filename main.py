c17f5ebd3d787dbd1a0b64ffda285e7ac8354fce
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-24 14:02:29 +0200

    Add fixed positioning Add messageboxes Add input text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import random
 from tkinter import *
+from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageDraw, ImageFont
 
@@ -17,34 +18,48 @@ def watermark(file):
 
     # Creating draw object
     draw = ImageDraw.Draw(img)
+    draw_text = ImageDraw.Draw(txt)
 
     # Creating text and font object
-    text = "Test"
-    font = ImageFont.truetype('~/Library/Fonts/Arial.ttf', 27)
+    text = water_mark.get()
+    font = ImageFont.truetype('~/Library/Fonts/Arial.ttf', 32)
 
-    # Positioning Text
+    # Text size
     width, height = img.size
 
-    y = 200
-    for i in range(12):
-        x = random.randint(0, width-300)
-        y += random.randrange(0, int(height/8), 19)+random.randint(0, 100)
-        draw.text((x, y), text, fill=(255, 255, 255, 75), font=font)
+    # Lets position the texts
+    counter = 0
+    for i in range(20):
+        counter += width * height / 13.7
 
-    # Combining both layers and saving new image
-    watermarked = Image.alpha_composite(img, txt)
+        x = counter % width
+        y = counter // width
+
+        draw_text.text((x, y * 0.95), text,
+                       fill=(255, 255, 255, 100), font=font)
 
     # Saving the new image with a marked label
-    img.save(f"{filename}_marked.{extension}")
+    img = Image.alpha_composite(img, txt)
+    img = img.convert("RGB")
+    try:
+        img.save(f"{filename}_marked.{extension}")
+    except:
+        messagebox.showerror(
+            message="There was an error saving the image!")
+    else:
+        messagebox.showinfo(message="Image saved successfully.")
 
 
 # -------------------------- SELECT FILE ------------------------------ #
 
 
 def file_select():
-    file = askopenfilename(initialdir=os.getcwd, title="Select an Image", filetypes=[
-        ("PNG", "*.png"), ("JPG", "*.jpg"), ("JPEG", "*.jpeg"), ("WEBP", "*.webp")])
-    watermark(file)
+    if len(water_mark.get()) == 0:
+        messagebox.showwarning(message="Please enter a Watermark text.")
+    else:
+        file = askopenfilename(initialdir=os.getcwd, title="Select an Image", filetypes=[
+            ("PNG", "*.png"), ("JPG", "*.jpg"), ("JPEG", "*.jpeg"), ("WEBP", "*.webp")])
+        watermark(file)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -60,11 +75,19 @@ title_label = Label(
     text="This App let's you select a file and add a Watermark to it.")
 title_label.grid(column=0, row=0, pady=20)
 
+watermark_label = Label(text="Enter watermark text: ")
+watermark_label.grid(column=0, row=1)
+
 file_select_label = Label(text="Please select file to upload: ")
-file_select_label.grid(column=0, row=1)
+file_select_label.grid(column=0, row=3)
+
+# Inputs
+water_mark = Entry(width=20)
+water_mark.grid(column=0, row=2)
+water_mark.focus()
 
 # Buttons
 select_button = Button(text="Select", command=file_select)
-select_button.grid(column=1, row=1)
+select_button.grid(column=0, row=4)
 
 window.mainloop()
